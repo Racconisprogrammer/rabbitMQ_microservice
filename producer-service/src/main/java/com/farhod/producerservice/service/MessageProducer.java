89a9760db648a9ad8a91f853a0580b4cd4b21d5c
Farhod <wedsdsa22@gmail.com>
2024-09-17 18:56:37 +0500

    created project

---
+++ producer-service/src/main/java/com/farhod/producerservice/service/MessageProducer.java
@@ -0,0 +1,18 @@
+package com.farhod.producerservice.service;
+
+import com.farhod.producerservice.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducer {
+    private RabbitTemplate rabbitTemplate;
+
+    public MessageProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.QUEUE_NAME, message);
+    }
+}
